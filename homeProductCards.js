90391446f35f430fda87a4c0583df711445844c5
ankuaish <anku.kesharwani17@gmail.com>
2024-05-05 20:28:55 +0530

    final

--- homeProductCards.js
+++ homeProductCards.js
@@ -21,8 +21,8 @@ productClone.querySelector(".productImage").src = image;
 productClone.querySelector(".productImage").alt = name;
 productClone.querySelector(".productDescription").textContent = description;
 productClone.querySelector(".productStock").textContent = stock;
-productClone.querySelector(".productPrice").textContent = `${price}`;
-productClone.querySelector(".productActualPrice").textContent = `${price *4}`;
+productClone.querySelector(".productPrice").textContent = `₹${price}`;
+productClone.querySelector(".productActualPrice").textContent = `₹${price *4}`;
 
 productClone.querySelector(".stockElement").addEventListener("click", (event) =>{
     homeQuantityToggle(event, id, stock);
